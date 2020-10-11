46c6e69c80f759eede8fb2b39b23ee5cd460b5fc
anhlt <42168740+BryantShepherd@users.noreply.github.com>
2020-10-11 13:17:04 +0700

    Refactor: add controller/ folder - fix api test

--- routers/user.route.js
+++ routers/user.route.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const responseUtil = require("../utils/responseUtils");
-const { userService } = require("../services");
 const auth = require("../config/auth");
-const { BCRYPT_SALT_ROUND } = require("../config");
-const bcrypt = require("bcrypt");
-const { UniqueViolationError, ValidationError } = require("objection");
 const { HTTPErrorMessage } = require("../config");
+const userController = require("../controllers/user.controller");
+const passport = require("passport");
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
@@ -27,42 +25,8 @@ router.get("/err", (req, res, next) => {
   }
 });
 
-router.post("/login", async (req, res, next) => {
-  const { email, password } = req.body;
-  try {
-    let user = await userService.authenticate(email, password);
-    let token = auth.createUserToken(user);
-    return responseUtil.success(res, 200, { user, token });
-  } catch (err) {
-    next(err);
-  }
-});
+router.post("/auth/login", userController.loginController);
 
-router.post("/register", async (req, res, next) => {
-  const { name, email, password, role_id } = req.body;
-
-  let hashedPassword = bcrypt.hashSync(password, BCRYPT_SALT_ROUND);
-  let newUser = {
-    name,
-    email,
-    password: hashedPassword,
-    role_id,
-  };
-
-  try {
-    let user = await userService.createUser(newUser);
-    delete user.password;
-    return responseUtil.success(res, 201, user);
-  } catch (err) {
-    if (err instanceof UniqueViolationError) {
-      err.status = 409;
-      err.httpMessage = HTTPErrorMessage.EMAIL_ALREADY_EXISTED;
-    } else if (err instanceof ValidationError) {
-      err.status = 400;
-      err.httpMessage = HTTPErrorMessage.REQUIRED_FIELDS_MISSING;
-    }
-    next(err);
-  }
-});
+router.post("/auth/register", userController.registerController);
 
 module.exports = router;
--- test/api/user.test.js
+++ test/api/user.test.js
@@ -1,16 +1,22 @@
 const request = require("supertest");
 const app = require("../../app");
 const knex = require("../../databases/knex");
+const auth = require("../../config/auth");
 
+// Create `test_remind_clone` database before runnign the tests.
 beforeAll(async () => {
-  await knex.migrate.down();
   await knex.migrate.latest();
-  await knex.seed.run();
+  return knex.seed.run();
 });
 
+afterAll(async () => {
+  return knex.migrate.down();
+});
+
+let validToken = null;
 test("Log the right user in", async () => {
   const res = await request(app)
-    .post("/api/user/login")
+    .post("/api/user/auth/login")
     .send({
       email: "nagisa@gmail.com",
       password: "password",
@@ -18,11 +24,13 @@ test("Log the right user in", async () => {
     .set("content-type", "application/json");
   expect(res.status).toBe(200);
   expect(res.body.data).toBeDefined();
+  expect(res.body.data.token).toBeDefined();
+  validToken = res.body.data.token;
 });
 
 test("Keep the wrong users out", async () => {
   const res = await request(app)
-    .post("/api/user/login")
+    .post("/api/user/auth/login")
     .send({
       email: "definitely.wrong.email@gmail.com",
       password: "incorrectpassword",
@@ -34,7 +42,7 @@ test("Keep the wrong users out", async () => {
 });
 
 test("Can register user", async () => {
-  const res = await request(app).post("/api/user/register").send({
+  const res = await request(app).post("/api/user/auth/register").send({
     name: "Rick Sanchez",
     email: "rickboi@email.com",
     password: "password",
@@ -51,4 +59,27 @@ test("Can register user", async () => {
   expect(res.status).toBe(201);
   expect(res.body.data).toBeDefined();
   expect(res.body.data).toEqual(response);
+});
+
+describe("Access to protected resources", () => {
+  test("Authenticated user can access protected resource", async () => {
+    app.get("/test/protected", auth.jwtAuth(), (req, res) => {
+      return res.status(200).json({ data: "OK" });
+    });
+
+    const res = await request(app)
+      .get("/test/protected")
+      .set("Authorization", `Bearer ${validToken}`);
+
+    expect(res.status).toBe(200);
+    expect(res.body.data).toBe("OK");
+  });
+
+  test("Unauthenticated user can't access protected resource", async () => {
+    const res = await request(app)
+      .get("/test/protected")
+      .set("Authorization", `Bearer fake-token`);
+
+    expect(res.status).toBe(401);
+  });
 });
