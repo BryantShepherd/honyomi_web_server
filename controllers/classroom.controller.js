25f5a4d2b831590d2c1c56c93fad6c58e438719c
Nguyen Dinh Bien <bien.uet279@gmail.com>
2020-10-18 01:48:17 +0700

    add CRUD Classroom (no relationship)

--- controllers/classroom.controller.js
+++ controllers/classroom.controller.js
@@ -1,7 +1,117 @@
 const responseUtil = require("../utils/responseUtils");
 const { userService } = require("../services");
-const auth = require("../config/auth");
-const { BCRYPT_SALT_ROUND } = require("../config");
-const bcrypt = require("bcrypt");
-const { UniqueViolationError, ValidationError } = require("objection");
-const { HTTPErrorMessage } = require("../config");
+const Classroom = require("../models/Classroom");
+
+
+const getClassrooms = async (req, res, next) => {
+  try {
+    let query = req.query;
+    const classrooms = await Classroom.query()
+        .where(query);
+    return res.status(200).json(classrooms);
+  } catch (err) {
+    next(err);
+  }
+}
+
+const postClassroom = async (req, res, next) => {
+  try {
+    const {code, name, school} = req.body;
+    const classroom = await Classroom.query()
+        .insert({
+          code: code,
+          name: name,
+          school: school
+        });
+
+    return res.status(201).json(classroom);
+  } catch (err) {
+    next(err);
+  }
+}
+
+const middleware = async (req, res, next) => {
+  try {
+    const classroomId = parseInt(req.params.classroomId);
+    const classroom = await Classroom.query()
+        .findById(classroomId);
+
+    if (classroom) {
+      req.classroom = classroom;
+      req.classroomId = classroomId;
+      return next();
+    }
+    return res.sendStatus(404);
+  } catch (err) {
+    next(err);
+  }
+}
+
+const getClassroom = async (req, res, next) => {
+  try {
+    const classroom = req.classroom;
+    return res.status(200).json(classroom);
+  } catch (err) {
+    next(err);
+  }
+}
+
+const putClassroom = async (req, res, next) => {
+  try {
+    const classroom = req.classroom;
+
+    classroom.code = req.body.code;
+    classroom.school = req.body.school;
+    classroom.name = req.body.name;
+
+    const newClassroom = await Classroom.query()
+        .updateAndFetchById(req.classroomId, classroom)
+    return res.status(202).json(newClassroom);
+  } catch (err) {
+    next(err);
+  }
+}
+
+const patchClassroom = async (req, res, next) => {
+  try {
+    const classroom = req.classroom;
+
+    if (req.body.id) {
+      delete req.body.id;
+    }
+
+    Object.entries(req.body).forEach((item) => {
+      const key = item[0];
+      const value =item[1];
+      classroom[key] = value;
+    });
+
+    const newClassroom = await Classroom.query()
+        .patchAndFetchById(req.classroomId, classroom);
+
+    return res.status(202).json(newClassroom);
+  } catch (err) {
+    next(err);
+  }
+}
+
+const deleteClassroom = async (req, res, next) => {
+  try {
+    const classroom = await Classroom.query()
+        .deleteById(req.classroomId);
+
+    return res.sendStatus(202);
+  } catch (err) {
+    next(err);
+  }
+}
+
+module.exports = {
+  getClassrooms,
+  postClassroom,
+  middleware,
+  getClassroom,
+  putClassroom,
+  patchClassroom,
+  deleteClassroom
+}
--- routers/classroom.route.js
+++ routers/classroom.route.js
@@ -1,73 +1,17 @@
 const express = require("express");
 const router = express.Router();
+const classroomController = require("../controllers/classroom.controller");
 
 router.route("/")
-  .get((req, res) => {
-    return res.json(classrooms)
-  })
-  .post((req, res) => {
-    const classroom = req.body;
-    return res.status(201).json(classroom);
-  })
+  .get(classroomController.getClassrooms)
+  .post(classroomController.postClassroom)
 
-
-router.use('/:classroomId',(req, res, next) => {
-  const classroom = classrooms.find(classroom => classroom.id === parseInt(req.params.classroomId));
-  if (classroom) {
-    req.classroom = classroom;
-    return next();
-  }
-  return res.sendStatus(404);
-})
+router.use('/:classroomId', classroomController.middleware)
 
 router.route("/:classroomId")
-  .get((req, res) => {
-    const classroom = req.classroom;
-    return res.json(classroom);
-  })
-
-  .put((req, res) => {
-    let classroom = req.classroom;
-    classroom.code = req.body.code;
-    classroom.school = req.body.school;
-    return res.status(201).json(classroom);
-  })
-
-  .patch((req, res) => {
-    let classroom = req.classroom;
-    return res.status(201).json(classroom);
-  })
-
-  .delete((req, res) => {
-    let classroom = req.classroom;
-    return res.status(201).json(classroom);
-  })
-
-classrooms = [
-  {
-    id: 1,
-    code: '1a2b3c',
-    name: 'UI Class',
-    school: 'UET',
-    created_at: '',
-    updated_at: ''
-  },
-  {
-    id: 2,
-    code: '1a2b3d',
-    name: 'DBMS Class',
-    school: 'UET',
-    created_at: '',
-    updated_at: ''
-  },
-  {
-    id: 3,
-    code: '1a2b3f',
-    name: 'Web Class',
-    school: 'UET',
-    created_at: '',
-    updated_at: ''
-  }
-]
+  .get(classroomController.getClassroom)
+  .put(classroomController.putClassroom)
+  .patch(classroomController.patchClassroom)
+  .delete(classroomController.deleteClassroom)
 
 module.exports = router;
