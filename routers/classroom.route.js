d13f1c335d557eeb27ecd025bc83d58469c3769f
anhlt <42168740+tuananhlai@users.noreply.github.com>
2021-03-19 20:59:26 +0700

    fix: change update classroom to POST

--- routers/classroom.route.js
+++ routers/classroom.route.js
@@ -18,7 +18,7 @@ router.use('/:classroomId', classroomController.middleware)
 router
   .route("/:classroomId")
   .get(classroomController.getClassroom)
-  .put(classroomController.putClassroom)
+  .post(classroomController.putClassroom)
   .patch(classroomController.patchClassroom)
   .delete(classroomController.deleteClassroom);
 
